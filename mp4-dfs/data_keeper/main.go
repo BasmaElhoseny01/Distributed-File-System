1ad051a231023a7286a66c6c320e67558b79488d
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-03-13 19:27:34 +0200

    Added initial File

---
+++ mp4-dfs/data_keeper/main.go
@@ -0,0 +1,9 @@
+package make
+
+import "log"
+
+func main() {
+	// TODO (1) Send Alive pings to the master node
+	log.Println("Hello From Data Node 📑")
+
+}
---
+++ mp4-dfs/master_tracker/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func main() {
+	// TODO Thread to listen to alive pings from data keepers
+	log.Println("Hello From Master Node 😎")
+}
