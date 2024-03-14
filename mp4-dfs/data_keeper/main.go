357ab15316b2f78d2685368bfa57e6192906fe13
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2024-03-14 11:51:52 +0200

    fix import error

--- mp4-dfs/data_keeper/main.go
+++ mp4-dfs/data_keeper/main.go
@@ -1,9 +1,26 @@
-package make
+package main
 
-import "log"
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	Reg "mp4-dfs/schema/register"
+)
+
 
 func main() {
-	// TODO (1) Send Alive pings to the master node
-	log.Println("Hello From Data Node 📑")
+	fmt.Println("Hello From Data Node 📑")
+	// TODO (1) Register to the master node
+
+	serverPort := "localhost:5002"
+	connToMaster, err := grpc.Dial(serverPort, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer connToMaster.Close()
+
+	client := Reg.NewRegisterClient(connToMaster)
+	// TODO (2) Send Alive pings to the master node
+	
+
 
 }
