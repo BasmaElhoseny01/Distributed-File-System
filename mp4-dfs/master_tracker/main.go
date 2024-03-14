e9956639a5a7cb97f67ae2c5d00954d7ab2f14a3
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-03-14 14:39:49 +0200

    Update main.go

--- mp4-dfs/master_tracker/main.go
+++ mp4-dfs/master_tracker/main.go
@@ -3,27 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
-	data_lookup "mp4-dfs/master_tracker/data_lookup"
-	reg "mp4-dfs/schema/register"
 	"net"
 	"sync"
+
+	data_lookup "mp4-dfs/master_tracker/data_lookup"
+	reg "mp4-dfs/schema/register"
+
 	"google.golang.org/grpc"
 )
 
-// var data_node_lookup_table=NewDataNodeLookUpTable()
-// var myVariable MyStruct // Declaring myVariable globally within the package
 
 type masterServer struct {
 	reg.UnimplementedDataKeeperRegisterServiceServer
+	reg.UnimplementedHeartBeatServiceServer
 	data_node_lookup_table data_lookup.DataNodeLookUpTable
 }
 
+
+// DataKeepersNodes Registration Services rpc
 func (s *masterServer) Register(ctx context.Context, in *reg.DataKeeperRegisterRequest) (*reg.DataKeeperRegisterResponse, error) {
 	fmt.Println("Received: ", in.GetIp())
 	fmt.Println("Received: ", in.GetPort())
 
+
 	// Add the data node to the lookup table
-	node_id, err := s.data_node_lookup_table.AddDataNode(&data_lookup.DataNode{Id: ""})
+	node_id,err := s.data_node_lookup_table.AddDataNode(&data_lookup.DataNode{Id: ""})
 	if err == nil {
 		fmt.Printf("New Data Node '%s' added Successfully\n", node_id)
 	}
@@ -31,7 +35,15 @@ func (s *masterServer) Register(ctx context.Context, in *reg.DataKeeperRegisterR
 	return &reg.DataKeeperRegisterResponse{DataKeeperId: node_id}, nil
 }
 
-func handleClient() {
+// HeartBeat Registration Services rpc
+func (s *masterServer) AlivePing(ctx context.Context, in *reg.AlivePingRequest) (*reg.AlivePingResponse, error) {
+	fmt.Println("Received: ", in.GetDataKeeperId())
+	s.data_node_lookup_table.UpdateNodeStatus( in.GetDataKeeperId(),true)
+	return nil,nil
+}
+
+
+func handleClient(master *masterServer) {
 	fmt.Println("Handle Client")
 	// listen to the port
 	dataKeeper_listener, err := net.Listen("tcp", "localhost:5001")
@@ -42,14 +54,14 @@ func handleClient() {
 
 	// define our master server and register the service
 	s := grpc.NewServer()
-	reg.RegisterDataKeeperRegisterServiceServer(s, &masterServer{})
+	reg.RegisterDataKeeperRegisterServiceServer(s, master)
 	if err := s.Serve(dataKeeper_listener); err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("Handle Client")
 }
 
-func handleDataKeeper() {
+func handleDataKeeper(master *masterServer) {
 	fmt.Println("Handle Data Keeper")
 	// listen to the port
 	dataKeeper_listener, err := net.Listen("tcp", "localhost:5002")
@@ -61,10 +73,12 @@ func handleDataKeeper() {
 	// define our master server and register the service
 	s := grpc.NewServer()
 
-	reg.RegisterDataKeeperRegisterServiceServer(s, &masterServer{
-		data_node_lookup_table: data_lookup.NewDataNodeLookUpTable(),
-	})
+	// Register in New Node Registration Service
+	reg.RegisterDataKeeperRegisterServiceServer(s,master)
 
+	// Register in HeartBeat Service
+	reg.RegisterHeartBeatServiceServer(s,master)
+	
 	if err := s.Serve(dataKeeper_listener); err != nil {
 		fmt.Println(err)
 	}
@@ -74,21 +88,28 @@ func handleDataKeeper() {
 func main() {
 	// TODO Thread to listen to alive pings from data keepers
 	fmt.Println("Hello From Master Node 😎")
+	// Create Master Server
+	master:=masterServer{data_node_lookup_table:data_lookup.NewDataNodeLookUpTable()}
 	// TODO (1) Register to the master node
 	wg := sync.WaitGroup{}
 	// add 2 goroutines to the wait group
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		handleClient()
+		handleClient(&master)
 	}()
 	go func() {
 		defer wg.Done()
-		handleDataKeeper()
+		handleDataKeeper(&master)
 	}()
 
 	// wait for all goroutines to finish
 	wg.Wait()
+
+
+
+
+
 
 	// // listen to the port
 	// dataKeeper_listener, err := net.Listen("tcp", "localhost:5002")
